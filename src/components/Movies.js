ea3b93f46feaa8a025e5ed3db7658e532d5f0295
Lucas Montanari <Lmonty2011@gmail.com>
2021-02-10 10:58:22 -0800

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => {
+          return <div>
+            <h3>Title: {movie.title}</h3>
+            <h3>Time: {movie.time}</h3>
+            <ul>
+              {movie.genres.map(genre => <li>{genre}</li>)}
+            </ul>
+          </div>
+        })}
     </div>
   );
 };
